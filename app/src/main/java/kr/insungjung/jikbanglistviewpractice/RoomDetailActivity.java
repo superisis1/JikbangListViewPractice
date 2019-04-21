7648b94a2fee56fe975a18e2001ff9cf11fe4f33
Insung Jung <superisis@gmail.com>
2019-04-21 14:59:21 +0900

    룸 디테일 액티비티 생성

---
+++ app/src/main/java/kr/insungjung/jikbanglistviewpractice/RoomDetailActivity.java
@@ -0,0 +1,13 @@
+package kr.insungjung.jikbanglistviewpractice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RoomDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_room_detail);
+    }
+}
